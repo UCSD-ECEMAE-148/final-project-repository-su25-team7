21a6616bdecd0ed84e5a3613c316835a72cb1ec4
Owethumk <okheswa@ucsd.edu>
2025-09-05 08:30:16 -0700

    Update final_program_start.py

--- team7_lidar/launch/final_program_start.py
+++ team7_lidar/launch/final_program_start.py
@@ -1,1 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
 
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='team7_lidar',
+            executable='team7_lidar_executable',
+            output='screen',
+            emulate_tty=True),
+    ])
